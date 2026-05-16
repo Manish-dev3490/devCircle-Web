63fe16e4aa2a331aa350e1700f13d8567a6b4980
manish-dev3490 <kmanish87064@gmail.com>
2026-05-16 18:56:06 +0530

    did some changes in validation

--- src/utility/validation.js
+++ src/utility/validation.js
@@ -2,7 +2,15 @@ import validator from "validator";
 
 function dataValidation(email, password) {
 
-    
+  if (email.length === 0 ) throw {
+    field: "email",
+    message: "your email input is empty"
+  };
+
+  if (password.length === 0) throw {
+    field: "password",
+    message: "your password input is empty"
+  };
   if (!validator.isEmail(email)) {
 
     throw {
