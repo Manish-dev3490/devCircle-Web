3c1f67bb3dec8ae6cf18ef01472e066855ba531b
manish-dev3490 <kmanish87064@gmail.com>
2026-06-07 23:49:51 +0530

    changed some buttons in ui

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -50,12 +50,10 @@ const UserDetail = ({ data }) => {
             <div className="flex gap-3">
 
                 <button className="btn bg-base-300 btn-sm px-4 mx-4  py-4">
-                    Message
+                    Chat
                 </button>
 
-                <button className="btn btn-outline btn-sm mx-4">
-                    Remove
-                </button>
+                
 
             </div>
 
