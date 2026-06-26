cacb737f5f8c6c94fa0bdcb1dc79eb445e2c7e50
manish-dev3490 <kmanish87064@gmail.com>
2026-06-26 22:09:18 +0530

    fixed some styling

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -89,7 +89,7 @@ const Chat = () => {
     <div className="flex justify-center items-center min-h-screen bg-base-200">
       <div className="w-full max-w-md h-[550px] bg-base-100 rounded-2xl shadow-xl flex flex-col overflow-hidden">
         {/* Header */}
-        <div className="bg-blue-600 text-white text-center py-4 font-semibold text-lg">
+        <div className="bg-gray-600 text-white text-center py-4 font-semibold text-lg">
           Chat
         </div>
 
@@ -100,7 +100,7 @@ const Chat = () => {
           ) : (
             messages.map((msg, index) => (
               <div key={index} className="mb-3 p-2 rounded-lg bg-gray-100">
-                <p className="font-semibold text-blue-600">
+                <p className="font-semibold text-gray-600">
                   {msg.firstName + " " + msg.lastName}
                 </p>
 
@@ -122,7 +122,7 @@ const Chat = () => {
 
           <button
             onClick={sendMsg}
-            className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
+            className="bg-gray-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
           >
             Send
           </button>
